0bd9cd8f44b474a858751cfa6745fd5b1cc50f16
Sarah Evermore <sarah.evermore.02@gmail.com>
2024-08-29 14:11:02 +0200

    add visual toggles arguments to args::Args :3

--- src/args.rs
+++ src/args.rs
@@ -9,4 +9,13 @@ pub(crate) struct Args {
     pub logo_kind: LogoKind,
     #[arg(short = 'c', long = "fg-color", default_value = "solaara-gold")]
     pub fg_color: crate::FgColor,
+
+    #[command(flatten, next_help_heading = "Visual Toggles")]
+    pub visual_toggles: VisualToggles,
+}
+
+#[derive(Clone, Debug, Copy, clap::Args)]
+pub(crate) struct VisualToggles {
+    #[arg(long)]
+    pub hide_terminal_version: bool,
 }
