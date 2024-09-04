683eb97758042861b746b91f9d2433a4237d208e
Sarah Evermore <sarah.evermore.02@gmail.com>
2024-09-04 15:45:19 +0200

    move FgColor into crate::args :3

--- src/args.rs
+++ src/args.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use ratatui::style::Color;
 
 use crate::logos::LogoKind;
 
@@ -8,7 +9,7 @@ pub(crate) struct Args {
     #[arg(short, long = "logo", default_value = "shadow")]
     pub logo_kind: LogoKind,
     #[arg(short = 'c', long = "fg-color", default_value = "solaara-gold")]
-    pub fg_color: crate::FgColor,
+    pub fg_color: FgColor,
 
     #[command(flatten, next_help_heading = "Visual Toggles")]
     pub visual_toggles: VisualToggles,
@@ -18,4 +19,19 @@ pub(crate) struct Args {
 pub(crate) struct VisualToggles {
     #[arg(long)]
     pub hide_terminal_version: bool,
+}
+
+#[derive(Debug, clap::ValueEnum, Clone, Copy, PartialEq, Eq)]
+pub(crate) enum FgColor {
+    SolaaraGold,
+    LightMagenta,
+}
+
+impl From<FgColor> for Color {
+    fn from(value: FgColor) -> Self {
+        match value {
+            FgColor::SolaaraGold => Color::Rgb(255, 241, 164),
+            FgColor::LightMagenta => Color::LightMagenta,
+        }
+    }
 }
